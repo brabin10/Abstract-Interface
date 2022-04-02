2df15c3518655283f2c7a65a7b3b3f0e2b9df64c
rowbinr@yahoo.com0 <rowbinr@yahoo.com>
2022-04-01 21:37:50 -0600

    sample project 0

--- P0Demo/src/main/java/com/revature/daos/EmployeeDAO.java
+++ P0Demo/src/main/java/com/revature/daos/EmployeeDAO.java
@@ -6,12 +6,18 @@ import com.revature.models.Employee;
 
 public class EmployeeDAO {
 	
+	//For now, I'll have an ArrayList of Employee objects, which will act as our fake database
 	ArrayList<Employee> employeeList = new ArrayList<>();
 	
+	
+	// This TEMPORARY method will return the employees from the database
 	public ArrayList<Employee> getEmployees() {
 
+		
+		//first, fill the Arraylist with some data
 		employeeList.add(new Employee(1, "Spongebob", "Square"));
 		employeeList.add(new Employee(2, "Squidward", "Tentacles"));
+
 		return employeeList;
 		
 	
