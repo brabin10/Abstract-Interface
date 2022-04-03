2320b2afbe69ebc581e0f8f76c64b4a415ba93e7
rowbinr@yahoo.com0 <rowbinr@yahoo.com>
2022-04-03 00:51:56 -0600

    Printing name 100 times

---
+++ String/src/com/java/PrintingFoo100Times.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class PrintingFoo100Times {
+	public static void main(String[] args) {
+		//String str = "Foo";
+		
+		for (int i = 1; i<=100; i++) {
+			
+			System.out.println(i+". Foo");
+		}
+	}
+
+}
