ce3f77b41f3e112fe4371a7c21a9cd76be0c857a
rowbinr@yahoo.com0 <rowbinr@yahoo.com>
2022-04-05 17:17:56 -0600

    metadata

--- P0Demo/src/main/java/com/revature/Launcher.java
+++ P0Demo/src/main/java/com/revature/Launcher.java
@@ -1,19 +1,27 @@
 package com.revature;
 
+import java.util.ArrayList;
+
 import com.revature.models.Menu;
 
 public class Launcher {
 
 	public static void main(String[] args) {
 		
-		
 		//our main method will only have 2 lines of code...
 		
 		//Instantiate our Menu object
 		Menu menu = new Menu();
 		
-		//Use the Menu's displayMenu() method to use the Menu
-		// The power of abstraction, all the complicated menu code is in the displayMenu() method ( abstracted away from us)
+		//use the Menu's displayMenu() method to use the Menu
 		menu.displayMenu();
+		
+		//really clean main method! The power of abstraction - 
+		//all the complicated menu code is in the displayMenu() method (abstracted away from us)
+		
+		
+
+		
 	}
+	
 }
--- P0Demo/src/main/java/com/revature/models/Menu.java
+++ P0Demo/src/main/java/com/revature/models/Menu.java
@@ -1,71 +1,87 @@
 package com.revature.models;
-
 import java.util.ArrayList;
 import java.util.Scanner;
-
 import com.revature.daos.EmployeeDAO;
-
-// This Menu Class will have a method that displays a menu to the user that they can interact with : displayMenu()
-// Through this menu, the user can give inputs that will interact with the database
+//This Menu Class will have a method that displays a menu to the user that they can interact with: displayMenu()
+//Through this menu, the user can give inputs that will interact with the database
 public class Menu {
 	
-	// instantiating the EmployeeDAO object so that we can use it's methods
+	//instantiating an EmployeeDAO object so that we can use it's methods
 	EmployeeDAO eDAO = new EmployeeDAO();
-	
-	// All of the menu display options and control flow are contained within this method
+	//All of the menu display options and control flow are contained within this method
 	public void displayMenu() {
 		
-		boolean displayMenu = true; // We're going to use this to toggle whether the menu continues after user input
-		Scanner scan = new Scanner(System.in); // Scanner object to parse (read) user input
+		boolean displayMenu = true; //we're going to use this to toggle whether the menu continues after user input
+		Scanner scan = new Scanner(System.in); //Scanner object to parse (read) user input
 		
 		//pretty greeting
-		System.out.println("Welcome to the Krusty Krab Employee Management System");
+		System.out.println("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*");
+		System.out.println("WELCOME to the Krusty Krab Employee Management System");
+		System.out.println("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*");
 		
-		// display the menu as long as the displayMenu boolean is true
+		//display the menu as long as the displayMenu boolean is true
 		while(displayMenu) {
 			
-			// tell the user to choose an option
-			System.out.println("Choose an option");
+			//tell the user to choose an option
+			System.out.println("----------------");
+			System.out.println("CHOOSE AN OPTION");
+			System.out.println("----------------");
 			
 			
 			//list out the user's options for them to choose
 			System.out.println("1: get greeted");
 			System.out.println("2: exit the application");
-			System.out.println("2: Show all employees");
+			System.out.println("3: show all employees");
 			
-			//parse the user's input after they choose option, and put it in a  int variable
+			
+			//parse the user's input after they choose option, and put it in a int variable
 			int input = scan.nextInt();
-			scan.nextLine(); //without nextLine(), the application won't move to the next Line...so we need it!
+			scan.nextLine(); //without nextLine(), the application won't move to the next line... so we need it!
 			
-			// Switch statement- depending on the uer's input, do some functionality
+			//switch statement - depending on the user's input, do some functionality
 			switch(input) {
 			
 			case 1: {
-				System.out.println("Hello User");
+				System.out.println("Hello User!");
 				break;
 			}
 			
 			case 2: {
-				System.out.println("Bye user! come again soon");
-				displayMenu = false;  //remember, the while loop depends on displayMenu being true. So this ends the loop
+				System.out.println("Bye user! Come again soon.");
+				displayMenu = false; //remember, the while loop depends on displayMenu being true. so this ends the loop.
 				break;
 			}
 			
 			case 3: {
 				//call the getEmployees() method from the EmployeeDAO, put the results into a variable
 				ArrayList<Employee> employees = eDAO.getEmployees();
-				//print out the value of our employee variable
+
+				//print out the value of our employees variable
 				System.out.println(employees);
-				
+				//print out the values of our employees variables in a forEach
+				//I'm sure you all can find even cleaner ways to print this data out ;)
+				employees.forEach(employee -> {
+					System.out.println("----------------------");
+					System.out.println(employee.getEmployee_id());
+					System.out.println(employee.getFirst_name());
+					System.out.println(employee.getLast_name());
+					System.out.println("----------------------");
+					//Ben won't forget to show emplyoee.getRole()
+				});
+
+				break;
 			}
 			
 			default: {
-				// this default block will run if the user inputs a number that isn't one of the options
-				System.out.println("Plaase try again");
+				//this default block will run if the user inputs a number that isn't one of the options
+				System.out.println("What did you say?? Try again. Do better...");
 				break;
 			}
-			}  //Switch statement ends here
-		}  // While loop ends here
-	}  // Methods ends here
-
+			
+			} //switch statement ends here
+			
+		} //while loop ends here
+		
+	} //method ends here
+	
 } //class ends here
