61ecfc2c02abd19e107b804cf8376af4d79de378
rowbinr@yahoo.com0 <rowbinr@yahoo.com>
2022-04-08 13:31:29 -0600

    connecting database

---
+++ P0Demo/SQL_script/krusty_krab.sql
@@ -0,0 +1,33 @@
+--This SCHEMA will model krusty krab employees AND their roles
+--This will be a one to many relationship, one role can be assigned to many employees, employees can only have one role.
+
+CREATE TABLE roles (
+	role_id serial PRIMARY KEY,
+	role_title TEXT,
+	role_salary int
+);
+
+CREATE TABLE employees (
+	employee_id serial PRIMARY KEY,
+	first_name TEXT,
+	last_name TEXT,
+	role_id_fk int REFERENCES roles (role_id) --FOREIGN KEY: establishes a relationship BETWEEN the tables
+
+);
+
+INSERT INTO roles (role_title, role_salary)
+VALUES ('Manager', 100000),
+	   ('Fry Cook', 25000),
+	   ('Cashier', 20000),
+	   ('Marketing Director', 100000);
+	  
+SELECT * FROM roles;
+
+INSERT INTO employees (first_name, last_name, role_id_fk)
+VALUES ('Eugene', 'Krabs', 1),
+	   ('Spongebob', 'Squarepants', 2),
+	   ('Pete', 'Fishman', 2),
+	   ('Squidward', 'Tentacles', 3),
+	   ('Sheldon', 'Plankton', 4);
+	   
+SELECT * FROM employees;
